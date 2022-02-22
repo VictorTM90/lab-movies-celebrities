11270d1197b12d6f42908641239152fad7f15a4d
victor <victor.tm90@gmail.com>
2022-02-22 18:43:59 +0100

    it 6 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,8 +1,21 @@
-const router = require("express").Router()
-const MovieModel = require ("../models/Movie.model")
-
+const router = require("express").Router();
+const MovieModel = require("../models/Movie.model");
+const CelebrityModel = require("../models/Celebrity.model");
 
 //RUTAS
+router.get("/create", async (req, res, next) => {
+ 
+ 
+    try {
+    const allCelebrities = await CelebrityModel.find();
+    console.log(allCelebrities)
+    res.render("movies/new-movie.hbs", { allCelebrities });
+  } catch (err) {
+    next(err);
+  }
+
+
+});
 
 
 
@@ -10,7 +23,4 @@ const MovieModel = require ("../models/Movie.model")
 
 
 
-
-
-
-module.exports = router
+module.exports = router;
