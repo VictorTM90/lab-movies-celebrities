76b7049685e521deb48e3b5276c8e607a6b09cb9
Dani Calvente <danicalvente@hotmail.com>
2022-02-22 21:54:40 +0100

    all iterations done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -74,6 +74,48 @@ router.get("/:id",  (req, res, next) => {
  
 })
 
+router.post("/:id/delete", (req, res, next) => {
+
+  const {id} = req.params
+
+  MovieModel.findOneAndDelete(id)
+  .then ((response) => {
+    res.redirect("/movies")
+  })
+  .catch ((err) => {
+    next(err)
+  })
+
+})
+
+router.get ("/:id/edit", async (req, res, next) => {
+
+   try {
+  const {id} = req.params
+
+   const updatedMovie = await MovieModel.findById(id)
+   const allCellebrities = await CelebrityModel.find()
+   res.render("movies/edit-movie.hbs", {updatedMovie, allCellebrities})
+   }
+   catch (err) {
+     next(err)
+   }
+})
+
+router.post ("/:id", async (req, res, next) => {
+
+  try{
+     const {id} = req.params
+  const {title, genre, plot, cast} = req.body
+
+  const updatedMovie = await MovieModel.findByIdAndUpdate(id, {title, genre, plot, cast})
+  res.redirect(`/movies/${id}`)
+  }
+  catch (err) {
+    next(err)
+  }
+
+})
 
 
 
